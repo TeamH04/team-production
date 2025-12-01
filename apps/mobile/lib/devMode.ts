ff4189d116b4cc3537a4632335f6d329162f9dc1
k-sato <kyokke.matsu@gmail.com>
2025-12-01 13:14:50 +0900

    chore: 変数名変更

--- apps/mobile/lib/devMode.ts
+++ apps/mobile/lib/devMode.ts
@@ -1,4 +1,4 @@
-const DEV_LOGIN_ENV = process.env.EXPO_PUBLIC_ENABLE_DEV_LOGIN;
+const DEV_LOGIN_ENV = process.env.EXPO_PUBLIC_ENABLE_DEV_MODE;
 
 export const DEV_LOGIN_ENABLED = DEV_LOGIN_ENV === 'true';
 export const DEV_GUEST_FLAG_KEY = 'dev_guest_login';
