f16fdb55f9c3415294e1bec9e5c4669e8730ab80
k-sato <kyokke.matsu@gmail.com>
2025-12-02 09:47:05 +0900

    chore: 開発者モードをビルドモードによって管理する

--- apps/mobile/lib/devMode.ts
+++ apps/mobile/lib/devMode.ts
@@ -1,4 +1,6 @@
 const DEV_LOGIN_ENV = process.env.EXPO_PUBLIC_ENABLE_DEV_MODE;
 
-export const DEV_LOGIN_ENABLED = DEV_LOGIN_ENV === 'true';
+declare const __DEV__: boolean;
+
+export const DEV_LOGIN_ENABLED = DEV_LOGIN_ENV === 'true' && __DEV__;
 export const DEV_GUEST_FLAG_KEY = 'dev_guest_login';
